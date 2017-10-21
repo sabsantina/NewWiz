0bbdcbbe697a3ba9aa5f0747c8ae703e9828b706
TheyCallMeJQ <jaquesmyster@hotmail.com>
2017-10-21 05:56:22 -0400

    Pickups
    Implemented basic functionalities for spawning of items and spells, as well as the ability to pick them up. The PlayerInventory::OnTriggerEnter function is being wonky, though. It somehow has time to execute twice while the player is just coming into the trigger. This is annoying because it's making it impossible to get a good idea of how many health_potions are in the player's inventory. We keep incrementing by 2 instead of 1, it sucks. Dunno how to fix this.
    This was my output on my last attempt:
    Spells:
    Items:
    Item name: Health_Potion	Item Effect: Gain_Health	Item Quantity:	2
    Item name: Health_Potion	Item Effect: Gain_Health	Item Quantity:	2
    Item name: Health_Potion	Item Effect: Gain_Health	Item Quantity:	2
    Spells can be picked up, and the inventory will adjust accordingly, at least.

--- BasicMechanicsDemo/Assets/Scripts/Spell.cs
+++ BasicMechanicsDemo/Assets/Scripts/Spell.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider))]
 public class Spell : MonoBehaviour {
 	/**An enum variable for the spell name. See SpellName.cs for all spell names.*/
 	public SpellName m_SpellName { set; get;}
@@ -11,4 +12,10 @@ public class Spell : MonoBehaviour {
 	*It'd be simpler to just keep all the spells in the inventory from the get-go, but only show them to the player if they've been discovered.
 	*We can do the same basic thing with the items; if the player has 0 quantity of a given item, we won't show it.*/
 	public bool m_HasBeenDiscovered{ set; get;}
+
+	void Awake()
+	{
+		//ensure the box collider's isTrigger is set to true
+		this.GetComponent<BoxCollider> ().isTrigger = true;
+	}//end f'n void Awake()
 }
