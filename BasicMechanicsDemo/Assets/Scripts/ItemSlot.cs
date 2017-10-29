7d861afafa18d2bf2490f0d757abf784eeb28cca
sabsantina <sabsantina@gmail.com>
2017-10-29 17:51:38 -0400

    Inventory Menu update
    - Inventory items now added to inventory menu
    - Updates when item number is updated
    - Made ItemSlots buttons

---
+++ BasicMechanicsDemo/Assets/Scripts/ItemSlot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlot : MonoBehaviour 
+{
+	public ItemClass item;
+	private GameObject itemImage;
+	private Text itemNumber;
+	// Use this for initialization
+	void Start () {
+		foreach (Transform child in gameObject.transform) {
+			if (child.tag == "SlotImage") 
+			{
+				itemImage = child.gameObject;
+				break;
+			}
+		}
+		itemNumber = GetComponentInChildren<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void setNumber(int value)
+	{
+		itemNumber.text = "" + value;
+	}
+
+	public void setItemSprite(Sprite s)
+	{
+		itemImage.GetComponent<Image> ().sprite = s;
+	}
+
+	public Text getText()
+	{
+		return itemNumber;
+	}
+}
--- BasicMechanicsDemo/Assets/Scripts/Menu/InventoryMenu.cs
+++ BasicMechanicsDemo/Assets/Scripts/Menu/InventoryMenu.cs
@@ -4,7 +4,84 @@ using UnityEngine;
 
 public class InventoryMenu : Menu {
 	/**A reference to the player's inventory*/
-	private PlayerInventory m_PlayerInventory;
+	public PlayerInventory m_PlayerInventory;
+	public Dictionary<ItemClass, int> itemDic;
+	private ItemIcon m_ItemIcon;
+	public ItemSlot[] itemSlots;
+	private bool notActive;
+	void Start()
+	{
+		m_PlayerInventory = GetComponentInParent<PlayerInventory> ();
+		itemDic = m_PlayerInventory.m_ItemDictionary;
+		m_ItemIcon = GetComponentInParent<ItemIcon> ();
+		itemSlots = GetComponentsInChildren<ItemSlot> ();
+		notActive = true;
+	}
+
+	void Update()
+	{
+		if (gameObject.activeSelf) {
+			if (notActive) {
+				updateInventoryMenu ();
+				notActive = false;
+			}
+		}
+		else
+		{
+			notActive = true;
+		}
+		
+	}
+
+	public void updateInventoryMenu()
+	{
+		Debug.Log ("Update inventory");
+		if (itemDic != null) 
+		{
+			foreach (KeyValuePair<ItemClass, int> entry in itemDic) 
+			{
+				ItemSlot findInInventory = findItem (entry.Key);
+				if (findInInventory != null) 
+				{
+					findInInventory.setNumber (entry.Value);
+				} 
+				else 
+				{
+					ItemSlot empty = findEmptySlot ();
+					if (empty == null)
+						Debug.Log ("There are no empty slots");
+					empty.setNumber (entry.Value);
+					empty.item = entry.Key;
+					Sprite itemIcon = m_ItemIcon.getItemSpire (entry.Key.m_ItemName);
+					empty.setItemSprite (itemIcon);
+				}
+			}
+		}
+	}
 
 
+	ItemSlot findEmptySlot()
+	{
+		foreach (ItemSlot slot in itemSlots) 
+		{
+			if (slot.getText ().text == "")
+				return slot;
+		}
+
+		return null;
+	}
+
+	ItemSlot findItem(ItemClass current)
+	{
+		foreach (ItemSlot slot in itemSlots) 
+		{
+			if (slot.getText ().text != "") 
+			{
+				if (slot.item.m_ItemName == current.m_ItemName)
+					return slot;
+			}
+		}
+
+		return null;
+	}
 }
