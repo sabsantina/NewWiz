89bddf8077b52c6aac3d59586380593af4c209f0
TheyCallMeJQ <jaquesmyster@hotmail.com>
2017-11-15 04:57:16 -0500

    Simplification of Enemy Class
    Simplification of application of spell effects on enemy. No more series of booleans for each individual spell, i.e. freezetimer, isfrozen, shocktimer, isshocked. Now we have one universal timer for any given effect and one "active spell" that the enemy can be hit with.
    Also created a txt doc to explain how to add new spells.

--- BasicMechanicsDemo/Assets/Scripts/Character/Mobile/Enemy/Enemy.cs
+++ BasicMechanicsDemo/Assets/Scripts/Character/Mobile/Enemy/Enemy.cs
@@ -18,20 +18,20 @@ public class Enemy : MonoBehaviour {
 
 	/**A float to contain the value of the player's full health*/
     public readonly float ENEMY_FULL_HEALTH = 100.0f;
-	/**The time until the enemy thaws and can move again.*/
-	public float TIME_BEFORE_THAW = 2.5f;
-	/**The time until the enemy is no longer being shocked; at which point it can move again and stop hopping.*/
-	public float TIME_BEFORE_SHOCK_RELEASE = 1.25f;
-    /**Boolean to check if enemy can move. To be used for the IceBall effects*/
-	public bool m_IsFrozen;
-    /**Timer to be used for the freezing effect.*/
-    private float freeze_Timer = 0.0f;
-
-	/**A bool letting us know whether or not the enemy is shocked.*/
-	public bool m_IsShocked;
-
-	/**Time to be used for the shocking effect.*/
-	public float m_ShockTimer = 0.0f;
+//	/**The time until the enemy thaws and can move again.*/
+//	public float TIME_BEFORE_THAW = 2.5f;
+//	/**The time until the enemy is no longer being shocked; at which point it can move again and stop hopping.*/
+//	public float TIME_BEFORE_SHOCK_RELEASE = 1.25f;
+//    /**Boolean to check if enemy can move. To be used for the IceBall effects*/
+//	public bool m_IsFrozen;
+//    /**Timer to be used for the freezing effect.*/
+//    private float freeze_Timer = 0.0f;
+//
+//	/**A bool letting us know whether or not the enemy is shocked.*/
+//	public bool m_IsShocked;
+//
+//	/**Time to be used for the shocking effect.*/
+//	public float m_ShockTimer = 0.0f;
 	/**A variable to keep track of how many times we've incremented the shock timer by units of 0.5.
 	*The plan is for the enemy to move up/down every 0.5 seconds. So if the time before shock release is 3.0, the enemy should move a grand total of 6 times; three times upward, and three times downward.*/
 	public int m_ShockTimerIncrementor = 1;
@@ -48,12 +48,17 @@ public class Enemy : MonoBehaviour {
 	public bool m_CanMove = true;
 	/**A bool to let us know whether or not the enemy's alive.*/
 	public bool m_IsAlive = true;
+	/**The spell hitting the enemy*/
+	public SpellClass m_SpellHittingEnemy = null;
+	public SpellName m_SpellName;
+	/**A float to help us keep track of a timer to regulate any extra effects (i.e., freezing, shocking)*/
+	public float m_ExtraEffectTimer = 0.0f;
 
     void Start()
     {
 		m_Health = ENEMY_FULL_HEALTH;
-        m_IsFrozen = false;
-		this.m_IsShocked = false;
+//        m_IsFrozen = false;
+//		this.m_IsShocked = false;
 		this.m_EnemyStartHeight = this.transform.position.y;
     }
     void Update()
@@ -87,60 +92,67 @@ public class Enemy : MonoBehaviour {
 			Debug.Log (message);
         }
 
-
-		//if the enemy is either frozen or shocked...
-		if (m_IsFrozen || this.m_IsShocked) {
-			this.m_CanMove = false;
-			//...then stop the animator
-			this.gameObject.GetComponent<Animator> ().enabled = false;
-			//...if the enemy is specifically frozen...
-			if (m_IsFrozen) {
-				//...then increment the freeze timer and release the enemy after the alloted time.
-				freeze_Timer += Time.deltaTime;
-				if (freeze_Timer >= TIME_BEFORE_THAW) {
-					this.gameObject.GetComponent<Animator> ().enabled = true;
-					m_IsFrozen = false;
-				}//end if
-			}//end if
-			//else if the enemy is shocked...
-			else {
-				//...then increment the shock timer and release the enemy after the alloted time.
-				this.m_ShockTimer += Time.deltaTime;
-				//if the shock timer exceeds or is equal to the time of shock release (where the enemy is no longer being shocked)...
-				if (this.m_ShockTimer >= TIME_BEFORE_SHOCK_RELEASE) {
-					//...then release the enemy
-					this.gameObject.GetComponent<Animator> ().enabled = true;
-					this.m_IsShocked = false;
-					//...ensure the enemy is at the proper height
-					Vector3 position = this.transform.position;
-					position.y = this.m_EnemyStartHeight;
-					this.transform.position = position;
-				}//end if
-				//else if the shock timer is less than the time of shock release (where the enemy is still being shocked)...
-				else {
-					//...then move the enemy either up or down, with respect to the shock timer incrementor
-
-					//if the shock timer is greater-than or equal to the shock incrementor * shock jump frequency...
-					if (this.m_ShockTimer >= this.m_ShockTimerIncrementor * this.m_ShockJumpFrequency) {
-						//...then move the enemy either up or down
-
-						Vector3 position = this.transform.position;
-						//...where if the shock timer incrementor is odd, we move the enemy up
-						//...and if the shock timer incrementor is even, we move the enemy back down
-						position.y += (this.m_ShockTimerIncrementor % 2 == 1) ? this.m_ShockJumpDistance : -(this.m_ShockJumpDistance);
-						this.transform.position = position;
-
-						this.m_ShockTimerIncrementor++;
-					}//end if
-				}//end else
-			}//end else
-		}//end if
-		else {
-			this.m_CanMove = true;
-			this.m_ShockTimer = 0.0f;
-//			this.freeze_Timer = 0.0f;
-			this.m_ShockTimerIncrementor = 1;
+		/**If the enemy's been hit by a spell and the spell has a lasting effct on the enemy...*/
+		if (this.m_SpellHittingEnemy != null && this.m_SpellHittingEnemy.m_EffectDuration > 0.0f) {
+			//...then ensure the enemy suffers the extra infliction that comes with the spell they've been hit with
+			this.ExecuteExtraInfliction ();
 		}
+
+		return;
+//
+//		//if the enemy is either frozen or shocked...
+//		if (m_IsFrozen || this.m_IsShocked) {
+//			this.m_CanMove = false;
+//			//...then stop the animator
+//			this.gameObject.GetComponent<Animator> ().enabled = false;
+//			//...if the enemy is specifically frozen...
+//			if (m_IsFrozen) {
+//				//...then increment the freeze timer and release the enemy after the alloted time.
+//				freeze_Timer += Time.deltaTime;
+//				if (freeze_Timer >= TIME_BEFORE_THAW) {
+//					this.gameObject.GetComponent<Animator> ().enabled = true;
+//					m_IsFrozen = false;
+//				}//end if
+//			}//end if
+//			//else if the enemy is shocked...
+//			else {
+//				//...then increment the shock timer and release the enemy after the alloted time.
+//				this.m_ShockTimer += Time.deltaTime;
+//				//if the shock timer exceeds or is equal to the time of shock release (where the enemy is no longer being shocked)...
+//				if (this.m_ShockTimer >= TIME_BEFORE_SHOCK_RELEASE) {
+//					//...then release the enemy
+//					this.gameObject.GetComponent<Animator> ().enabled = true;
+//					this.m_IsShocked = false;
+//					//...ensure the enemy is at the proper height
+//					Vector3 position = this.transform.position;
+//					position.y = this.m_EnemyStartHeight;
+//					this.transform.position = position;
+//				}//end if
+//				//else if the shock timer is less than the time of shock release (where the enemy is still being shocked)...
+//				else {
+//					//...then move the enemy either up or down, with respect to the shock timer incrementor
+//
+//					//if the shock timer is greater-than or equal to the shock incrementor * shock jump frequency...
+//					if (this.m_ShockTimer >= this.m_ShockTimerIncrementor * this.m_ShockJumpFrequency) {
+//						//...then move the enemy either up or down
+//
+//						Vector3 position = this.transform.position;
+//						//...where if the shock timer incrementor is odd, we move the enemy up
+//						//...and if the shock timer incrementor is even, we move the enemy back down
+//						position.y += (this.m_ShockTimerIncrementor % 2 == 1) ? this.m_ShockJumpDistance : -(this.m_ShockJumpDistance);
+//						this.transform.position = position;
+//
+//						this.m_ShockTimerIncrementor++;
+//					}//end if
+//				}//end else
+//			}//end else
+//		}//end if
+//		else {
+//			this.m_CanMove = true;
+//			this.m_ShockTimer = 0.0f;
+////			this.freeze_Timer = 0.0f;
+//			this.m_ShockTimerIncrementor = 1;
+//		}
 	}//end f'n void Update()
 
 
@@ -156,36 +168,40 @@ public class Enemy : MonoBehaviour {
      *Should make it abstract when we add a variety of enemies.*/
 	public void ApplySpellEffects(SpellName spell_name)
 	{
+		this.m_SpellHittingEnemy = new SpellClass ();
 		switch ((int)spell_name) {
 		case (int)SpellName.Fireball:
 			{
 				this.AffectHealth (-10.0f);
+
+				this.m_SpellHittingEnemy = this.m_SpellHittingEnemy.GenerateInstance (SpellName.Fireball);
 				break;
 			}//end case Fireball
         case (int)SpellName.Iceball:
             {
                 this.AffectHealth(-2.0f);
-                this.m_IsFrozen = true;
-                freeze_Timer = 0.0f;
-                break;
+                
+				this.m_SpellHittingEnemy = this.m_SpellHittingEnemy.GenerateInstance (SpellName.Iceball);
+				break;
             }//end case Iceball
 		case (int)SpellName.Thunderball:
 			{
 				this.AffectHealth (-5.0f);
-				this.m_IsShocked = true;
-				this.m_ShockTimer = 0.0f;
 				this.m_ShockTimerIncrementor = 1;
 
+				this.m_SpellHittingEnemy = this.m_SpellHittingEnemy.GenerateInstance (SpellName.Thunderball);
 				break;
 			}//end case Thunderball
 		case (int)SpellName.Thunderstorm:
 			{
 				this.AffectHealth (-40.0f);
-				this.m_IsShocked = true;
 				if (this.m_ShockTimerIncrementor > 10) {
-					this.m_ShockTimer = 0.0f;
 					this.m_ShockTimerIncrementor = 1;
 				}
+
+				//No point making a section for thunderstorm; thunderball can be used for the same purpose.
+				//We'll make an instance of thunderstorm but let the switch fall through to thunderball
+				this.m_SpellHittingEnemy = this.m_SpellHittingEnemy.GenerateInstance (SpellName.Thunderstorm);
 				break;
 			}
 		default:
@@ -193,7 +209,112 @@ public class Enemy : MonoBehaviour {
 				//Impossible, right now
 				break;
 			}
-		}
+		}//end switch
+		this.m_SpellName = this.m_SpellHittingEnemy.m_SpellName;
+
 	}//end f'n void ApplySpellEffects(SpellName)
+
+	/**A function to inflict the enemy with an extra effect (beyond losing health)*/
+	private void ExecuteExtraInfliction()
+	{
+		/*
+	There's three phases to a spell with a duration greater than 0; 
+		1. the start of the timer
+		2. the time where the timer has started but hasn't finished
+		3. the end of the timer
+		*/
+		switch ((int)this.m_SpellHittingEnemy.m_SpellName) {
+		case (int)SpellName.Fireball:
+			{
+				//do nothing
+				break;
+			}
+		case (int)SpellName.Iceball:
+			{
+				//Freeze the enemy for Iceball.duration and then let them go
+
+				//Phase 1
+				if (this.m_ExtraEffectTimer == 0.0f) {
+					//...stop the enemy from moving
+					this.m_CanMove = false;
+					//...and stop the animator
+					this.gameObject.GetComponent<Animator> ().enabled = false;
+					this.m_ExtraEffectTimer += Time.deltaTime;
+				} 
+				//Phase 2
+				else if (0.0f < this.m_ExtraEffectTimer && this.m_ExtraEffectTimer < this.m_SpellHittingEnemy.m_EffectDuration) {
+					//Increment timer
+					this.m_ExtraEffectTimer += Time.deltaTime;
+				} 
+				//Phase 3
+				//if the spell effect duration <= the spell effect timer
+				else {
+					//Unfreeze the enemy
+					this.m_CanMove = true;
+					this.gameObject.GetComponent<Animator> ().enabled = true;
+
+					//Reset the effect timer
+					this.m_ExtraEffectTimer = 0.0f;
+					//Reset the spell we were hit with
+					this.m_SpellHittingEnemy = null;
+				}//end else
+
+				break;
+			}//end case Iceball
+			//Let thunderstorm case fall to thunderball
+		case (int)SpellName.Thunderstorm:
+		case (int)SpellName.Thunderball:
+			{
+				//Shock the enemy for Thunderball.duration and let them go
+
+				//Phase 1
+				if (this.m_ExtraEffectTimer == 0.0f) {
+					//Stop the enemy moving
+					//...stop the enemy from moving
+					this.m_CanMove = false;
+					//...and stop the animator
+					this.gameObject.GetComponent<Animator> ().enabled = false;
+					this.m_ExtraEffectTimer += Time.deltaTime;
+				} 
+				//Phase 2
+				else if (0.0f < this.m_ExtraEffectTimer && this.m_ExtraEffectTimer < this.m_SpellHittingEnemy.m_EffectDuration) {
+					//if the shock timer is greater-than or equal to the shock incrementor * shock jump frequency...
+					if (this.m_ExtraEffectTimer >= this.m_ShockTimerIncrementor * this.m_ShockJumpFrequency) {
+						//...then move the enemy either up or down
+
+						Vector3 position = this.transform.position;
+						//...where if the shock timer incrementor is odd, we move the enemy up
+						//...and if the shock timer incrementor is even, we move the enemy back down
+						position.y += (this.m_ShockTimerIncrementor % 2 == 1) ? this.m_ShockJumpDistance : -(this.m_ShockJumpDistance);
+						this.transform.position = position;
+
+						this.m_ShockTimerIncrementor++;
+					}
+					this.m_ExtraEffectTimer += Time.deltaTime;
+				}
+				//Phase 3
+				//if the spell effect duration <= the spell effect timer
+				else
+				{
+					//...then release the enemy
+					this.gameObject.GetComponent<Animator> ().enabled = true;
+					this.m_CanMove = true;
+					//...ensure the enemy is at the proper height
+					Vector3 position = this.transform.position;
+					position.y = this.m_EnemyStartHeight;
+					this.transform.position = position;
+
+					//Reset the shock timer incrementor
+					this.m_ShockTimerIncrementor = 1;
+					//Reset the effect timer
+					this.m_ExtraEffectTimer = 0.0f;
+					//Reset the spell we were hit with
+					this.m_SpellHittingEnemy = null;
+				}//end if
+
+				break;
+			}//end case thunderball
+		}//end switch
+	}//end f'n void ExecuteExtraInfliction()
 
 }
