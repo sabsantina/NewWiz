bcbdca8ff87c58b15abab79d71712c6511c54a57
TheyCallMeJQ <jaquesmyster@hotmail.com>
2017-11-16 21:31:45 -0500

    Update

--- BasicMechanicsDemo/Assets/Scripts/Character/Mobile/Enemy/EnemyTypes/EnemyInfantry.cs
+++ BasicMechanicsDemo/Assets/Scripts/Character/Mobile/Enemy/EnemyTypes/EnemyInfantry.cs
@@ -18,8 +18,9 @@ public class EnemyInfantry : DefaultEnemy {
 	* Note that in the MovementPattern class, we have a function executing movement executing in Update, and what it does depends on the MovementPatternState. So all we need do to affect movement is change the value of the movement pattern state.*/
 	public override void Move ()
 	{
+		Debug.Log (this.m_MovementPattern.IsPlayerDetectedInPatrolRegion ());
 		//If the player is detected...
-		if (this.m_MovementPattern.IsPlayerDetected ()) {
+		if (this.m_MovementPattern.IsPlayerDetectedInPatrolRegion ()) {
 			//...then an infantry enemy should chase the player
 			this.m_MovementPattern.m_MovementPatternState = MovementPatternState.CHASE_PLAYER;
 		} 
@@ -29,4 +30,23 @@ public class EnemyInfantry : DefaultEnemy {
 			this.m_MovementPattern.m_MovementPatternState = MovementPatternState.ROAM;
 		}
 	}//end f'n void Move()
+
+	/**A function to have the enemy apply their given attack on the player.*/
+	public override void Attack()
+	{
+		//To be overridden in children classes
+	}
+
+	/**A function to execute and control the enemy's death animation*/
+	public override void Die()
+	{
+		//To be overridden in children classes
+	}
+
+	/**A function to apply a given spell's effects on the enemy, including damage.*/
+	public override void ApplySpellEffect (SpellClass spell)
+	{
+		//To be overridden in children classes
+		//Note: this is virtual because certain spells may affect certain enemies differently
+	}
 }
--- BasicMechanicsDemo/Assets/Scripts/Character/Mobile/Enemy/MovementPattern/MovementPattern.cs
+++ BasicMechanicsDemo/Assets/Scripts/Character/Mobile/Enemy/MovementPattern/MovementPattern.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class MovementPattern : MobileCharacter {
 
 	/**The enemy patrol and detection radius*/
-	[SerializeField] EnemyDetectionArea m_DetectionArea;
+	[SerializeField] EnemyPatrolRegion m_PatrolRegion;
+
+//	[SerializeField] Animator m_EnemyAnimator;
 
 	/**The state of the movement pattern.*/
 	public MovementPatternState m_MovementPatternState = MovementPatternState.ROAM;
@@ -14,24 +16,50 @@ public class MovementPattern : MobileCharacter {
 	/**A bool to let us know whether or not the player's been detected.*/
 	public bool m_PlayerDetected;
 	/**The velocity at which an enemy chases a player.*/
-	public float m_ChaseVelocity = 10.0f;
+	public float m_ChaseVelocity = 200.0f;
+
+	public float m_CurrentVelocity = 0.0f;
 
 	// Use this for initialization
 	void Start () {
 		this.m_Direction = new Vector3(Random.Range(-100.0f, 100.0f), 0.0f, Random.Range(-100.0f, 100.0f));
 		this.m_MaximalVelocity = 7.5f;
+//		this.m_Animator = this.m_EnemyAnimator;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		this.ExecutePatternState ();
+		this.m_CurrentVelocity = this.m_Direction.magnitude;
+		Debug.Log (m_CurrentVelocity);
 	}
 
 	/**A function to check whether or not the player was detected within the enemy's patrol area*/
-	public bool IsPlayerDetected()
+	public bool IsPlayerDetectedInPatrolRegion()
 	{
-		return false;
+		return this.m_PatrolRegion.m_PlayerInRegion;
 	}
+
+	/**A function to check whether the enemy is leaving its alloted patrol region.*/
+	private bool MovingLeavesPatrolRegion(Vector3 displacement)
+	{
+		Vector3 vector_difference = this.m_PatrolRegion.transform.position - this.transform.position;
+		float distance_from_center = Vector3.Magnitude((this.transform.position + displacement) - this.m_PatrolRegion.transform.position);
+		float detection_area_radius = this.m_PatrolRegion.GetComponent<SphereCollider> ().radius * this.m_PatrolRegion.transform.lossyScale.x;
+		//if the distance between the enemy and the center of the detection area is greater than the radius of the
+		//detection area (minus a little extra interior padding)...
+		if (distance_from_center > (detection_area_radius - this.m_PatrolRegion.m_InteriorPadding)) {
+			//...then the enemy is beyond the detection area
+			return true;
+		}//end if
+		//else if the distance between the enemy and the center of the detection area is less than the radius of the
+		//detection area (minus a little extra interior padding)...
+		else {
+			//...then we're fine
+			return false;
+		}//end else
+	}//end f'n bool isLeavingDetectionArea()
+
 
 	/**Move the gameobject with respect to the current movement pattern.
 	*Note: Updates animator parameters immediately following execution of the actual movement.*/
@@ -43,6 +71,11 @@ public class MovementPattern : MobileCharacter {
 			{
 				Vector3 current_position = this.transform.position;
 				Vector3 displacement = Vector3.Normalize (this.m_Direction) * this.m_MaximalVelocity * Time.deltaTime;
+				if (this.MovingLeavesPatrolRegion (displacement)) {
+					displacement = Vector3.Normalize (this.m_PatrolRegion.transform.position - this.transform.position) * this.m_MaximalVelocity;
+					//Update direction
+					this.m_Direction = displacement;
+				}
 				this.transform.position = current_position + displacement;
 				break;
 			}
@@ -56,16 +89,18 @@ public class MovementPattern : MobileCharacter {
 		case (int)MovementPatternState.MOVE_TOWARDS_PLAYER:
 			{
 				Vector3 current_position = this.transform.position;
-				Vector3 vector_to_player = Vector3.Normalize (this.m_Player.gameObject.transform.position - current_position) * this.m_MaximalVelocity * Time.deltaTime;
-				this.transform.position = current_position + vector_to_player;
+				Vector3 vector_to_player = Vector3.Normalize (this.m_Player.gameObject.transform.position - current_position) * this.m_MaximalVelocity;
+				this.m_Direction = vector_to_player;
+				this.transform.position = current_position + (this.m_Direction * Time.deltaTime);
 				break;
 			}
 		//If the enemy's movement pattern is set to chase the player, at a greater-than-normal velocity
 		case (int)MovementPatternState.CHASE_PLAYER:
 			{
 				Vector3 current_position = this.transform.position;
-				Vector3 vector_to_player = Vector3.Normalize (this.m_Player.gameObject.transform.position - current_position) * this.m_ChaseVelocity * Time.deltaTime;
-				this.transform.position = current_position + vector_to_player;
+				Vector3 vector_to_player = Vector3.Normalize (this.m_Player.gameObject.transform.position - current_position) * this.m_ChaseVelocity;
+				this.m_Direction = vector_to_player;
+				this.transform.position = current_position + (this.m_Direction * Time.deltaTime);
 				break;
 			}
 		}//end switch
