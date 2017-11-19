3f8872742c583692ef9fbb3a0cb1752298ddb561
Unknown <bbrthanasis@gmail.com>
2017-11-19 02:36:30 -0500

    Auto stash before merge of "Tom" and "origin/master"

---
+++ BasicMechanicsDemo/Assets/Scripts/Character/Mobile/Enemy/EnemyTypes/InfantryUnits/ArmoredSoldier.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmoredSoldier : EnemyInfantry {
+    /**The health of an armored soldier*/
+    public float m_ArmoredSoldierHealth = 60.0f;
+    /**The attack damage of an armored soldier*/
+    public float m_ArmoredSoldierAttackDamage = 15.0f;
+    /**The string value of the name of the sorting layer*/
+    public string sortingLayerName;
+
+    // Use this for initialization
+    void Start () {
+        this.SetHealth();
+        this.SetAttackDamageValue();
+        this.gameObject.GetComponentInChildren<SpriteRenderer>().sortingLayerName = sortingLayerName;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.gameObject.GetComponentInChildren<SpriteRenderer>().sortingOrder = Mathf.RoundToInt(this.GetComponent<Transform>().transform.position.z * 100f) * -1;
+        this.Move();
+        if (this.IsPlayerInRangeOfAttack())
+        {
+            this.Attack();
+        }
+        else
+        {
+            this.m_AttackPattern.m_AttackPatternState = AttackPatternState.DO_NOTHING;
+        }
+    }
+
+    public override void SetHealth()
+    {
+        this.m_Health = this.m_ArmoredSoldierHealth;
+    }
+
+    public override void SetAttackDamageValue()
+    {
+        this.m_AttackDamageValue = this.m_ArmoredSoldierAttackDamage;
+    }
+
+    public override float GetAttackDamageValue()
+    {
+        return this.m_AttackDamageValue;
+    }
+}
