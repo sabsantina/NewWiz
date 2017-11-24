c39a1506d0ff254e771fbb8f2613ff359208c1ca
sabsantina <sabsantina@gmail.com>
2017-11-23 20:34:59 -0500

    Created scripts for biography

---
+++ BasicMechanicsDemo/Assets/Scripts/BioButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BioButton : MonoBehaviour {
+	[SerializeField] Sprite bioSprite;
+	[SerializeField] Sprite inventorySprite;
+	[SerializeField] GameObject bioCanvas;
+
+	Sprite buttonImage;
+
+	// Use this for initialization
+	void Start () 
+	{
+		buttonImage = GetComponent<Image> ().sprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void buttonClick()
+	{
+		if (buttonImage == bioSprite) 
+		{
+			bioCanvas.SetActive (true);
+			buttonImage = inventorySprite;
+		} 
+		else 
+		{
+			bioCanvas.SetActive (false);
+			buttonImage = bioSprite;
+		}
+	}
+}
---
+++ BasicMechanicsDemo/Assets/Scripts/Biography.cs
@@ -0,0 +1,38 @@
+﻿#define testing
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Biography : MonoBehaviour 
+{
+	#if testing
+	string part1 = "";
+
+	#endif
+	
+	Text bioText;
+	RectTransform rectTrans;
+	// Use this for initialization
+	void Start () 
+	{
+		bioText = gameObject.GetComponentInChildren<Text> ();
+		rectTrans = gameObject.GetComponent<RectTransform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		#if testing
+		if(Input.GetKeyDown(KeyCode.F1))
+		{
+			
+		}
+		#endif
+	}
+
+
+
+}
