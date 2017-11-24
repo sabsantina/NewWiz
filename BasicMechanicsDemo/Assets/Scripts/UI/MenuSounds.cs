ea3022df5071b00a339d00f9ce72eda394ff8636
sabsantina <sabsantina@gmail.com>
2017-11-23 22:42:13 -0500

    Added audio to inventory menu
    -Need to fix the hotkey settings

---
+++ BasicMechanicsDemo/Assets/Scripts/UI/MenuSounds.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSounds : MonoBehaviour 
+{
+	[SerializeField] AudioClip buttonHover;
+	[SerializeField] AudioClip buttonClick;
+	[SerializeField] AudioClip errorSound;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public AudioClip getSoundByType(bool clicked)
+	{
+		if (clicked)
+			return buttonClick;
+		else
+			return buttonHover;
+				
+	}
+
+	public AudioClip getErrorSound()
+	{
+		return errorSound;
+	}
+}
