b265cf1433d5ed73b334f2c10ea7965d012ea6d6
Unknown <bbrthanasis@gmail.com>
2017-11-26 02:29:38 -0500

    Added implementation to Teine boss

--- BasicMechanicsDemo/Assets/Scripts/AttackHit.cs
+++ BasicMechanicsDemo/Assets/Scripts/AttackHit.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClawHit : MonoBehaviour{
-    private Animator clawAnimator;
+public class AttackHit : MonoBehaviour{
+    private Animator hitAnimator;
 	// Use this for initialization
 	void Start () {
-        clawAnimator = this.GetComponent<Animator>();
+        hitAnimator = this.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (clawAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Destroy")){
+        if (hitAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Destroy")){
             GameObject.Destroy(this.gameObject);
         }
 
