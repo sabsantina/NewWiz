de44d67358175a58d3636078cb5cd242d809d754
Albert <albertnaggear@gmail.com>
2017-11-26 23:38:37 -0500

    RPGTalk fix, maybe?

---
+++ BasicMechanicsDemo/Assets/Scripts/Managers/TextManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextManager : MonoBehaviour
+{
+    [SerializeField] private RPGTalk _RPGTalkContainer;
+    [SerializeField] private List<RPGTalkArea> talkAreas;
+
+    // Use this for initialization
+    private void Start()
+    {
+        _RPGTalkContainer = Instantiate(_RPGTalkContainer);
+        _RPGTalkContainer.GetComponent<Canvas>().worldCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        foreach (RPGTalkArea talkArea in talkAreas)
+        {
+            talkArea.rpgtalkTarget = _RPGTalkContainer;
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
