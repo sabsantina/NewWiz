691b1b3b9e74c5f6e71f488b6f9b751e61859c63
Albert <albertnaggear@gmail.com>
2017-11-27 10:48:04 -0500

    temp commit, rebuilding RPGTalk

--- BasicMechanicsDemo/Assets/Scripts/Managers/TextManager.cs
+++ BasicMechanicsDemo/Assets/Scripts/Managers/TextManager.cs
@@ -6,16 +6,18 @@ public class TextManager : MonoBehaviour
 {
     [SerializeField] private RPGTalk _rpgTalkContainer;
     [SerializeField] private List<RPGTalkArea> _talkAreas;
-
+    [SerializeField] private List<TextAsset> _textAssets;
     // Use this for initialization
     private void Start()
     {
-       // _rpgTalkContainer = Instantiate(_rpgTalkContainer);
-//        _rpgTalkContainer.GetComponent<Canvas>().worldCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
-//        foreach (RPGTalkArea talkArea in _talkAreas)
-//        {
-//            talkArea.rpgtalkTarget = _rpgTalkContainer;
-//        }
+        _rpgTalkContainer = Instantiate(_rpgTalkContainer);
+        _rpgTalkContainer.GetComponent<Canvas>().worldCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        int x;
+        foreach (RPGTalkArea talkArea in _talkAreas)
+        {
+            talkArea.rpgtalkTarget = _rpgTalkContainer;
+            talkArea.txtToParse = _textAssets[0];
+        }
     }
 
     // Update is called once per frame
