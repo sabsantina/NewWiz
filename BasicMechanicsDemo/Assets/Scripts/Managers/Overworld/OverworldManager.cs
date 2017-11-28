b67f99a849a302a02f5ed874d967f0b461cb90b7
Unknown <bbrthanasis@gmail.com>
2017-11-28 12:18:38 -0500

    Added soljas in overworld. Fixed a couple of bugs

--- BasicMechanicsDemo/Assets/Scripts/Managers/Overworld/OverworldManager.cs
+++ BasicMechanicsDemo/Assets/Scripts/Managers/Overworld/OverworldManager.cs
@@ -15,12 +15,24 @@ public class OverworldManager : MonoBehaviour {
 	[SerializeField] private GameObject m_WolfDen3;
 	/**A gameobject to tell us where we want to spawn some wolves*/
 	[SerializeField] private GameObject m_WolfDen4;
+    /**A gameobject to tell us where we want to spawn some soldiers*/
+    [SerializeField] private GameObject m_SoldierDen1;
+    /**A gameobject to tell us where we want to spawn some soldiers*/
+    [SerializeField] private GameObject m_SoldierDen2;
+    /**A gameobject to tell us where we want to spawn some soldiers*/
+    [SerializeField] private GameObject m_SoldierDen3;
+    /**A gameobject to tell us where we want to spawn some soldiers*/
+    [SerializeField] private GameObject m_SoldierDen4;
+    /**A gameobject to tell us where we want to spawn some soldiers*/
+    [SerializeField] private GameObject m_SoldierDen5;
 
-	/**The number of wolves to spawn, per den.*/
-	private int m_NumberofWolvesToSpawn = 5;
+    /**The number of wolves to spawn, per den.*/
+    private int m_NumberofWolvesToSpawn = 5;
+    /**The number of soldiers to spawn, per den.*/
+    private int m_NumberofSoldiersToSpawn = 3;
 
-	/**The place where we want to spawn the rooster king.*/
-	[SerializeField] private GameObject m_RoosterKingSpawnPoint;
+    /**The place where we want to spawn the rooster king.*/
+    [SerializeField] private GameObject m_RoosterKingSpawnPoint;
 
 	public static bool m_RoosterKingDead = false;
 
@@ -31,8 +43,13 @@ public class OverworldManager : MonoBehaviour {
 		this.SpawnInWolves (this.m_WolfDen2.transform.position);
 		this.SpawnInWolves (this.m_WolfDen3.transform.position);
 		this.SpawnInWolves (this.m_WolfDen4.transform.position);
+        this.SpawnInSoldiers(this.m_SoldierDen1.transform.position);
+        this.SpawnInSoldiers(this.m_SoldierDen2.transform.position);
+        this.SpawnInSoldiers(this.m_SoldierDen3.transform.position);
+        this.SpawnInSoldiers(this.m_SoldierDen4.transform.position);
+        this.SpawnInSoldiers(this.m_SoldierDen5.transform.position);
 
-		if (!m_RoosterKingDead) {
+        if (!m_RoosterKingDead) {
 //			Debug.Log ("Calling function");
 			GameObject enemy = this.m_Spawner.SpawnEnemy (EnemyName.ROOSTER_KING);
 			enemy.GetComponentInChildren<DefaultEnemy> ().m_Spawner = this.m_Spawner;
@@ -56,4 +73,16 @@ public class OverworldManager : MonoBehaviour {
 			wolf.transform.position = (position + Vector3.forward * index * 0.5f);
 		}
 	}
+
+    private void SpawnInSoldiers(Vector3 position)
+    {
+        GameObject enemy_container = new GameObject();
+        enemy_container.name = EnemyName.ARMORED_SOLDIER + " Container";
+        for (int index = 0; index < this.m_NumberofSoldiersToSpawn; index++)
+        {
+            GameObject armoredSoldier = this.m_Spawner.SpawnEnemy(EnemyName.ARMORED_SOLDIER, enemy_container.transform);
+            armoredSoldier.GetComponentInChildren<DefaultEnemy>().m_Spawner = this.m_Spawner;
+            armoredSoldier.transform.position = (position + Vector3.forward * index * 0.5f);
+        }
+    }
 }
