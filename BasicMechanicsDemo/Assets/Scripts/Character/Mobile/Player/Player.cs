743588e935b0224d7db8e18fde87de259bfbdca1
Unknown <bbrthanasis@gmail.com>
2017-11-27 23:05:31 -0500

    Fixed bugs with Soirhbeach scene and scene transitions.

--- BasicMechanicsDemo/Assets/Scripts/Character/Mobile/Player/Player.cs
+++ BasicMechanicsDemo/Assets/Scripts/Character/Mobile/Player/Player.cs
@@ -224,7 +224,8 @@ public class Player : MonoBehaviour, ICanBeDamagedByMagic {
                     break;
                 }
 		}//end switch
-		this.transform.position = position_to_spawn_player;
+        //Will change back to getting the transform.position set when the testing stops. This works weird when you start at a specific scene other than main menu.
+		this.m_PlayerRespawnPosition = position_to_spawn_player;
 	}
 
 	void Update()
